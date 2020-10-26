ab0d6287eb06097ba395c34d4a1e5b8526cb85fb
SirNoura <65782557+SirNoura@users.noreply.github.com>
2020-10-26 16:56:22 +0000

    Initial commit

---
+++ app/src/main/java/ipca/roject/alldayproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package ipca.roject.alldayproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
