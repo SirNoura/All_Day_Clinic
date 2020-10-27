2e2879d8f68c150ea693322ff78e4f2b23a5f8a2
gituser2001 <64527203+gituser2001@users.noreply.github.com>
2020-10-27 15:11:24 +0000

    error in splash screen

--- app/src/main/java/ipca/roject/alldayproject/SplashScreen.kt
+++ app/src/main/java/ipca/roject/alldayproject/SplashScreen.kt
@@ -15,8 +15,7 @@ class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-
+        setContentView(R.layout.activity_splash_screen)//aqui
 
         Handler().postDelayed({
 
